2945b67e61a3a422a6a9b29f0a9812786fecbfec
klimmeEkb <151643633+klimmeEkb@users.noreply.github.com>
2023-11-28 16:39:31 +0300

    Add files via upload

---
+++ circles.py
@@ -0,0 +1,41 @@
+import sys
+import random
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from designe.py import designe as des
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Круги на полях')
+        self.button = des(QPushButton(self))
+        self.do_paint = False
+        self.button.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        r = random.randint(10, 100)
+        qp.drawEllipse(random.randint(10, 200), random.randint(10, 200), r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
